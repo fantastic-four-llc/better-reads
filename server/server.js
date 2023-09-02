b2e05d99ab4bdea139ee7ee4984804e3766e1b2a
David Yoon <DYoonity@users.noreply.github.com>
2023-09-01 21:15:33 -0400

    Initial scaffolding

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+
+// require controllers
+// const controller = require('./controllers.js');
+
+const app = express();
+const PORT = 3005;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, '../src')));
+
+// catch all
+app.use('*', (req, res) => {
+    res.sendStatus(404).send('What the dog doin?');
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+    const defaultError = {
+        log: 'Express error handler caught unknown middleware error',
+        status: 500,
+        message: { err: 'An error occurred. RIP.' },
+    };
+    const errorObj = Object.assign({}, defaultError, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /.(css|scss)$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.mp4$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'video',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+        },
+        proxy: {
+            '/test': 'http://localhost:3005', // path and port here
+        },
+    },
+};
