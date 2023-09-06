a4ee033c26f87e7e36780db6ec5f8c191f066557
Moiz Rauf <moizrauf@Moizs-Air.home>
2023-09-06 14:24:39 -0400

    fixed crashing of server, created dbActions module, got dbActions.addBook to work, got bookControllerSQL.addBook to use it successfully

--- server/sql/dbActions.js
+++ server/sql/dbActions.js
@@ -9,8 +9,16 @@ dbActions.addBook = async (title, author, genre) => {
     RETURNING book_id, title, author, genre;`;
     try {
         const result = await db.query(query, values);
+        // console.log('response: ', result);
+        // console.log('response type: ', typeof result);
+        // console.log('response.rows: ', result.rows);
+        // console.log('response.rows[0]: ', result.rows[0]);
+        return result.rows[0];
     }
     catch(err){
         console.log('err: ', err);
+        return err;
     };
 }
+
+module.exports = dbActions;
